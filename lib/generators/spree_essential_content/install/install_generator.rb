9a8930777e8b50680e843e62a7392f2bd8e79a76
mileer <mileer>
2014-09-28 22:46:08 +0400

    New changes

--- lib/generators/spree_essential_content/install/install_generator.rb
+++ lib/generators/spree_essential_content/install/install_generator.rb
@@ -5,13 +5,13 @@ module SpreeEssentialContent
       class_option :auto_run_migrations, :type => :boolean, :default => false
 
       def add_javascripts
-        append_file 'vendor/assets/javascripts/spree/frontend/all.js', "//= require frontend/spree_essential_content"
-        vendorend_file 'vendor/assets/javascripts/spree/backend/all.js', "//= require backend/spree_essential_content"
+        append_file 'vendor/assets/javascripts/spree/frontend/all.js', "//= require frontend/spree_essential_content\n"
+        append_file 'vendor/assets/javascripts/spree/backend/all.js', "//= require backend/spree_essential_content\n"
       end
 
       def add_stylesheets
-        inject_into_file 'vendor/assets/stylesheets/spree/frontend/all.css', " *= require frontend/spree_essential_content", :before => /\*\//, :verbose => true
-        inject_into_file 'vendor/assets/stylesheets/spree/backend/all.css', " *= require backend/spree_essential_content", :before => /\*\//, :verbose => true
+        inject_into_file 'vendor/assets/stylesheets/spree/frontend/all.css', " *= require frontend/spree_essential_content\n", :before => /\*\//, :verbose => true
+        inject_into_file 'vendor/assets/stylesheets/spree/backend/all.css', " *= require backend/spree_essential_content\n", :before => /\*\//, :verbose => true
       end
 
       def add_migrations
